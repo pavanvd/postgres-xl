aaae6df46df423b49f8819f957aa81b96601ea0c
Michael Paquier <michael@otacoo.com>
2012-08-23 13:01:02 +0900

    Fix GTM startup with "*" set as listen_addresses on OSX
    On OSX, GTM was not able to start with default parameter values.
    This had side effects on regressions with make check, making impossible
    to run a simple default regression test.

--- src/gtm/proxy/proxy_main.c
+++ src/gtm/proxy/proxy_main.c
@@ -596,9 +596,7 @@ main(int argc, char *argv[])
 		}
 	}
 
-/*
 	ListenAddresses = strdup(GTM_PROXY_DEFAULT_HOSTNAME);
-*/
 	GTMProxyPortNumber = GTM_PROXY_DEFAULT_PORT;
 	GTMProxyWorkerThreads = GTM_PROXY_DEFAULT_WORKERS;
 
@@ -819,9 +817,15 @@ main(int argc, char *argv[])
 	{
 		int			success = 0;
 
+		if (strcmp(ListenAddresses, "*") == 0)
+			status = StreamServerPort(AF_UNSPEC, NULL,
+									  (unsigned short) GTMProxyPortNumber,
+									  ListenSocket, MAXLISTEN);
+		else
 			status = StreamServerPort(AF_UNSPEC, ListenAddresses,
 									  (unsigned short) GTMProxyPortNumber,
 									  ListenSocket, MAXLISTEN);
+
 		if (status == STATUS_OK)
 			success++;
 		else
