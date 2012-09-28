291ea9c6eaa88304347aabcdd83fc2feb4e19d9e
Andrei Martsinchyk <Andrei.Martsinchyk@gmail.com>
2012-09-28 13:31:10 +0300

    Bugfix: added missing break in a switch

--- src/gtm/main/main.c
+++ src/gtm/main/main.c
@@ -1471,6 +1471,7 @@ ProcessTransactionCommand(Port *myport, GTM_MessageType mtype, StringInfo messag
 
 		case MSG_BKUP_TXN_BEGIN_GETGXID:
 			ProcessBkupBeginTransactionGetGXIDCommand(myport, message);
+			break;
 
 		case MSG_TXN_BEGIN_GETGXID_AUTOVACUUM:
 			ProcessBeginTransactionGetGXIDAutovacuumCommand(myport, message);
