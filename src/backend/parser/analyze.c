116ed14b1207575f49be6d410b69b22a325acd41
Tom Lane <tgl@sss.pgh.pa.us>
2012-11-11 19:56:16 -0500

    Check for stack overflow in transformSetOperationTree().
    Since transformSetOperationTree() recurses, it can be driven to stack
    overflow with enough UNION/INTERSECT/EXCEPT clauses in a query.  Add a
    check to ensure it fails cleanly instead of crashing.  Per report from
    Matthew Gerber (though it's not clear whether this is the only thing
    going wrong for him).
    
    Historical note: I think the reasoning behind not putting a check here in
    the beginning was that the check in transformExpr() ought to be sufficient
    to guard the whole parser.  However, because transformSetOperationTree()
    recurses all the way to the bottom of the set-operation tree before doing
    any analysis of the statement's expressions, that check doesn't save it.

--- src/backend/parser/analyze.c
+++ src/backend/parser/analyze.c
@@ -43,6 +43,7 @@
 #include "utils/tqual.h"
 #endif
 #include "catalog/pg_type.h"
+#include "miscadmin.h"
 #include "nodes/makefuncs.h"
 #include "nodes/nodeFuncs.h"
 #include "optimizer/var.h"
@@ -1641,6 +1642,9 @@ transformSetOperationTree(ParseState *pstate, SelectStmt *stmt,
 	bool		isLeaf;
 
 	Assert(stmt && IsA(stmt, SelectStmt));
+
+	/* Guard against stack overflow due to overly complex set-expressions */
+	check_stack_depth();
 
 	/*
 	 * Validity-check both leaf and internal SELECTs for disallowed ops.
