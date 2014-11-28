4f96d160e2162973c0eb4409b1f4c64a879467b8
Pavan Deolasee <pavan.deolasee@gmail.com>
2014-11-28 16:52:18 +0530

    Fix test case xc_FQS_join and its expected output

--- src/test/regress/sql/xc_FQS_join.sql
+++ src/test/regress/sql/xc_FQS_join.sql
@@ -1,54 +1,19 @@
 -- This file contains testcases for JOINs, it does not test the expressions
 -- create the tables first
--- A function to create table on specified nodes 
-create or replace function cr_table(tab_schema varchar, nodenums int[], distribution varchar, cmd_suffix varchar)
-returns void language plpgsql as $$
-declare
-	cr_command	varchar;
-	nodes		varchar[];
-	nodename	varchar;
-	nodenames_query varchar;
-	nodenames 	varchar;
-	node 		int;
-	sep			varchar;
-	tmp_node	int;
-	num_nodes	int;
-begin
-	nodenames_query := 'SELECT node_name FROM pgxc_node WHERE node_type = ''D'''; 
-	cr_command := 'CREATE TABLE ' || tab_schema || ' DISTRIBUTE BY ' || distribution || ' TO NODE ';
-	for nodename in execute nodenames_query loop
-		nodes := array_append(nodes, nodename);
-	end loop;
-	nodenames := '';
-	sep := '';
-	num_nodes := array_length(nodes, 1);
-	foreach node in array nodenums loop
-		tmp_node := node;
-		if (tmp_node < 1 or tmp_node > num_nodes) then
-			tmp_node := tmp_node % num_nodes;
-			if (tmp_node < 1) then
-				tmp_node := num_nodes; 
-			end if;
-		end if;
-		nodenames := nodenames || sep || nodes[tmp_node];
-		sep := ', ';
-	end loop;
-	cr_command := cr_command || nodenames;
-	if (cmd_suffix is not null) then
-		cr_command := cr_command  || ' ' || cmd_suffix;
-	end if;
-	execute cr_command;
-end;
-$$;
 
-select cr_table('tab1_rep (val int, val2 int)', '{1, 2, 3}'::int[], 'replication', NULL);
+select create_table_nodes('tab1_rep (val int, val2 int)', '{1, 2, 3}'::int[], 'replication', NULL);
 insert into tab1_rep (select * from generate_series(1, 5) a, generate_series(1, 5) b);
-select cr_table('tab2_rep', '{2, 3, 4}'::int[], 'replication', 'as select * from tab1_rep');
-select cr_table('tab3_rep', '{1, 3}'::int[], 'replication', 'as select * from tab1_rep');
-select cr_table('tab4_rep', '{2, 4}'::int[], 'replication', 'as select * from tab1_rep');
-select cr_table('tab1_mod', '{1, 2, 3}'::int[], 'modulo(val)', 'as select * from tab1_rep');
-select cr_table('tab2_mod', '{2, 4}'::int[], 'modulo(val)', 'as select * from tab1_rep');
-select cr_table('tab3_mod', '{1, 2, 3}'::int[], 'modulo(val)', 'as select * from tab1_rep');
+select create_table_nodes('tab2_rep', '{2, 3, 4}'::int[], 'replication', 'as select * from tab1_rep');
+select create_table_nodes('tab3_rep', '{1, 3}'::int[], 'replication', 'as select * from tab1_rep');
+select create_table_nodes('tab4_rep', '{2, 4}'::int[], 'replication', 'as select * from tab1_rep');
+select create_table_nodes('tab1_mod', '{1, 2, 3}'::int[], 'modulo(val)', 'as select * from tab1_rep');
+select create_table_nodes('tab2_mod', '{2, 4}'::int[], 'modulo(val)', 'as select * from tab1_rep');
+select create_table_nodes('tab3_mod', '{1, 2, 3}'::int[], 'modulo(val)', 'as select * from tab1_rep');
+select create_table_nodes('single_node_rep_tab', '{1}'::int[], 'replication', 'as select * from tab1_rep limit 0');
+select create_table_nodes('single_node_mod_tab', '{1}'::int[], 'modulo(val)', 'as select * from tab1_rep limit 0');
+-- populate single node tables specially
+insert into single_node_rep_tab values (1, 2), (3, 4);
+insert into single_node_mod_tab values (1, 2), (5, 6);
 
 -- Join involving replicated tables only, all of them should be shippable
 select * from tab1_rep, tab2_rep where tab1_rep.val = tab2_rep.val and
@@ -136,8 +101,111 @@ explain (verbose on, nodes off, costs off, num_nodes on) select * from tab1_mod 
 select * from tab1_mod, tab3_mod where tab1_mod.val = tab3_mod.val and tab1_mod.val = 1;
 explain (verbose on, nodes off, costs off) select * from tab1_mod, tab3_mod
 			where tab1_mod.val = tab3_mod.val and tab1_mod.val = 1;
+
+-- JOIN between relations which are results of subqueries should obey same rules
+-- as normal tables
+-- replicated subqueries 
+select * from (select * from tab1_rep) t1 natural join (select * from tab2_rep) t2
+			where t1.val > 1 and t1.val < 4
+			order by t1.val, t1.val2;
+explain (costs off, verbose on, nodes off, num_nodes on)
+	select * from (select * from tab1_rep) t1 natural join (select * from tab2_rep) t2
+				where t1.val > 1 and t1.val < 4
+				order by t1.val, t1.val2;
+select * from (select avg(val2), val from tab1_rep group by val) t1 natural join
+				(select avg(val2), val from tab2_rep group by val) t2
+			order by 1, 2;
+explain (costs off, verbose on, nodes off, num_nodes on)
+	select * from (select avg(val2), val from tab1_rep group by val) t1 natural join
+					(select avg(val2), val from tab2_rep group by val) t2
+				order by 1, 2;
+-- replicated and distributed subqueries 
+select * from (select avg(val2), val from tab1_mod group by val) t1 natural join
+				(select avg(val2), val from tab1_rep group by val) t2
+			where t1.val = 3;
+explain (costs off, verbose on, nodes off)
+	select * from (select avg(val2), val from tab1_mod group by val) t1 natural join
+					(select avg(val2), val from tab1_rep group by val) t2
+				where t1.val = 3;
+-- distributed subqueries
+select * from (select avg(val2), val from tab1_mod group by val) t1 natural join
+				(select avg(val2), val from tab3_mod group by val) t2
+			where t1.val = 3;
+explain (costs off, verbose on, nodes off)
+	select * from (select avg(val2), val from tab1_mod group by val) t1 natural join
+					(select avg(val2), val from tab3_mod group by val) t2
+			where t1.val = 3;
+
+-- OUTER joins, we insert some data in existing tables for testing OUTER join
+-- OUTER join between two replicated tables is shippable if they have a common
+-- datanode.
+insert into tab1_rep values (100, 200);
+insert into tab2_rep values (3000, 4000);
+select * from tab1_rep left join tab2_rep on (tab1_rep.val = tab2_rep.val and tab1_rep.val2 = tab2_rep.val2)
+			where tab2_rep.val = tab2_rep.val2 or tab2_rep.val is null
+			order by tab1_rep.val, tab1_rep.val2;
+explain (costs off, verbose on, nodes off)
+select * from tab1_rep left join tab2_rep on (tab1_rep.val = tab2_rep.val and tab1_rep.val2 = tab2_rep.val2)
+			where tab1_rep.val = tab1_rep.val2 or tab2_rep.val is null
+			order by tab1_rep.val, tab1_rep.val2;
+-- FULL OUTER join
+select * from tab1_rep full join tab2_rep on (tab1_rep.val < tab2_rep.val and tab1_rep.val2 = tab2_rep.val2) 
+					where tab1_rep.val > 5 or tab2_rep.val > 5
+					order by tab1_rep.val, tab2_rep.val, tab1_rep.val2, tab2_rep.val2;
+explain (costs off, verbose on, nodes off)
+select * from tab1_rep full join tab2_rep on (tab1_rep.val < tab2_rep.val and tab1_rep.val2 = tab2_rep.val2)
+					where tab1_rep.val > 5 or tab2_rep.val > 5
+					order by tab1_rep.val, tab2_rep.val, tab1_rep.val2, tab2_rep.val2;
+-- OUTER join between two distributed tables is shippable if it's an equi-join
+-- on the distribution columns, such that distribution columns are of same type
+-- and the relations are distributed on same set of nodes
+insert into tab1_mod values (100, 200);
+insert into tab3_mod values (3000, 4000);
+select * from tab1_mod left join tab3_mod on (tab1_mod.val = tab3_mod.val and tab1_mod.val2 = tab3_mod.val2)
+			where tab3_mod.val = tab3_mod.val2 or tab3_mod.val is null
+			order by tab1_mod.val, tab1_mod.val2;
+explain (costs off, verbose on, nodes off)
+select * from tab1_mod left join tab3_mod on (tab1_mod.val = tab3_mod.val and tab1_mod.val2 = tab3_mod.val2)
+			where tab3_mod.val = tab3_mod.val2 or tab3_mod.val is null
+			order by  tab1_mod.val, tab1_mod.val2;
+-- JOIN condition is not equi-join on distribution column, join is not shippable
+select * from tab1_mod left join tab3_mod using (val2)
+			where (tab1_mod.val = tab1_mod.val2 and tab3_mod.val = tab3_mod.val2) or tab3_mod.val is null
+			order by tab1_mod.val, tab1_mod.val2, tab3_mod.val2;
+explain (costs off, verbose on, nodes off)
+select * from tab1_mod left join tab3_mod using (val2)
+			where (tab1_mod.val = tab1_mod.val2 and tab3_mod.val = tab3_mod.val2) or tab3_mod.val is null
+			order by  tab1_mod.val, tab1_mod.val2, tab3_mod.val2;
+-- OUTER join between replicated and distributed tables is shippable if the
+-- the replicated table is available on all the datanodes where outer side is
+-- distributed
+select * from tab1_mod left join tab1_rep on (tab1_mod.val < tab1_rep.val and tab1_mod.val2 = tab1_rep.val2)
+			where tab1_mod.val >= 5
+			order by tab1_mod.val, tab1_mod.val2, tab1_rep.val, tab1_rep.val2;
+explain (costs off, verbose on, nodes off)
+select * from tab1_mod left join tab1_rep on (tab1_mod.val < tab1_rep.val and tab1_mod.val2 = tab1_rep.val2)
+			where tab1_mod.val >= 5
+			order by tab1_mod.val, tab1_mod.val2, tab1_rep.val, tab1_rep.val2;
+-- OUTER side is replicated and inner is distributed, join is not shippable,
+-- just check the EXPLAIN outputs.
+explain (costs off, verbose on, nodes off)
+select * from tab1_mod right join tab1_rep on (tab1_mod.val > tab1_rep.val and tab1_mod.val2 = tab1_rep.val2)
+			where tab1_rep.val >= 5;
+explain (costs off, verbose on, nodes off)
+select * from tab1_rep left join tab1_mod on (tab1_mod.val > tab1_rep.val and tab1_mod.val2 = tab1_rep.val2)
+			where tab1_rep.val >= 5;
+-- Any join involving a distributed and replicated node each located on a single
+-- and same node should be shippable
+select * from single_node_rep_tab natural full outer join single_node_mod_tab order by val, val2;
+explain (costs off, verbose on, nodes off)
+select * from single_node_rep_tab natural full outer join single_node_mod_tab order by val, val2;
+
 -- DMLs involving JOINs are not FQSed
-explain (verbose on, nodes off, costs off) update tab1_mod set val2 = 1000 from tab2_mod 
+-- We need to just make sure that FQS is not kicking in. But the JOINs can still
+-- be reduced by JOIN reduction optimization. Turn this optimization off so as
+-- to generate plans independent of number of nodes in the cluster.
+set enable_remotejoin to false;
+explain (costs off, verbose on, nodes off) update tab1_mod set val2 = 1000 from tab2_mod 
 		where tab1_mod.val = tab2_mod.val and tab1_mod. val2 = tab2_mod.val2;
 explain (verbose on, nodes off, costs off) delete from tab1_mod using tab2_mod
 		where tab1_mod.val = tab2_mod.val and tab1_mod.val2 = tab2_mod.val2;
@@ -152,4 +220,7 @@ drop table tab3_rep;
 drop table tab4_rep;
 drop table tab1_mod;
 drop table tab2_mod;
-drop function cr_table(varchar, int[], varchar, varchar);
+drop table tab3_mod;
+drop table single_node_mod_tab;
+drop table single_node_rep_tab;
+
